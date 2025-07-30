57b28fee7a6dfa783a0d16fa13aca167d03fa166
nancy <963991839@qq.com>
2025-07-30 10:33:27 +0800

    perf: 我的页面

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components'
 import { AuthWrapper } from '../../utils/authWrapper'
 import Taro, { useLoad } from '@tarojs/taro'
 import './index.scss'
@@ -10,7 +10,90 @@ function ProfilePage() {
 
   return (
     <View className='profile'>
-      <Text>我的页面</Text>
+      {/* 用户信息区域 */}
+      <View className='user-card'>
+        <Image
+          src='https://picsum.photos/300/400'
+          className='avatar'
+          mode='aspectFill'
+        />
+        <View className='user-meta'>
+          <Text className='nickname'>用户名</Text>
+          <Text className='member-level'>黄金会员</Text>
+        </View>
+      </View>
+
+      {/* 订单入口区域 */}
+      <View className='action-grid'>
+        <View className='action-item'>
+          <View className='action-badge'>2</View>
+          {/* <Image
+            src='https://picsum.photos/300/400'
+            className='action-icon'
+            mode='aspectFill'
+          /> */}
+          <Text className='action-label'>商品订单</Text>
+        </View>
+        <View className='action-item'>
+          <View className='action-badge'>5</View>
+          {/* <Image
+            src='https://picsum.photos/300/400'
+            className='action-icon'
+            mode='aspectFill'
+          /> */}
+          <Text className='action-label'>权益订单</Text>
+        </View>
+      </View>
+
+      {/* 功能列表 */}
+      <View className='feature-list'>
+        {/* <View className='feature-item'>
+          <Image
+            src='https://picsum.photos/300/400'
+            className='feature-icon'
+          />
+          <Text className='feature-title'>商品订单</Text>
+          <Image
+            src='https://picsum.photos/300/400'
+            className='arrow'
+          />
+        </View>
+        <View className='feature-item'>
+          <Image
+            src='https://picsum.photos/300/400'
+            className='feature-icon'
+          />
+          <Text className='feature-title'>权益订单</Text>
+          <Image
+            src='https://picsum.photos/300/400'
+            className='arrow'
+          />
+        </View> */}
+        <View className='feature-item'>
+          <Image
+            src='https://picsum.photos/300/400'
+            className='feature-icon'
+          />
+          <Text className='feature-title'>在线客服</Text>
+          <Text className='feature-desc'>24小时为您服务</Text>
+          <Image
+            src='https://picsum.photos/300/400'
+            className='arrow'
+          />
+        </View>
+        <View className='feature-item'>
+          <Image
+            src='https://picsum.photos/300/400'
+            className='feature-icon'
+          />
+          <Text className='feature-title'>客服电话</Text>
+          <Text className='feature-desc'>400-888-8888</Text>
+          <Image
+            src='https://picsum.photos/300/400'
+            className='arrow'
+          />
+        </View>
+      </View>
     </View>
   )
 }
